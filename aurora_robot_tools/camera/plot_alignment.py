46eaa153b59a90ca480e045a0dc8101596272153
Lina Scholz <lina.sofie.scholz@gmail.com>
2024-10-20 17:16:15 +0200

    save intermediate status with ne plot which visualizes alignment (not perfect yet)

--- aurora_robot_tools/camera/plot_alignment.py
+++ aurora_robot_tools/camera/plot_alignment.py
@@ -12,6 +12,7 @@ import pandas as pd
 import h5py
 from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 #%%
 
@@ -59,12 +60,71 @@ if not os.path.exists(plot_path):
     os.makedirs(plot_path)
 
 # save plot
-fig.savefig(plot_path + "/" + unique_filename, format='png')
+# fig.savefig(plot_path + "/" + unique_filename, format='png')
 
 # --------------------------
 
 # TODO: create overlapping plot
+# Grid size
+grid_size = 6
 
+# List of misalignments (y-values correspond to batches, x-values to pressing tool position)
+missalignments = [(x/100, y/100) for x, y in zip(x_values, y_values)] # in um
+cat_missalign = [missalignments[0:6], missalignments[6:12], missalignments[12:18], missalignments[18:24], missalignments[24:30], missalignments[30:36]]
+
+cat_radius = df_images["s6_r"].to_list() # cathode radius
+cat_radius = [x / 1000 for x in cat_radius]
+radii_cat = [cat_radius[0:6], cat_radius[6:12], cat_radius[12:18], cat_radius[18:24], cat_radius[24:30], cat_radius[30:36]]
+
+ano_radius = df_images["s4_r"].to_list() # anode radius
+ano_radius = [x / 1000 for x in ano_radius]
+radii_ano = [ano_radius[0:6], ano_radius[6:12], ano_radius[12:18], ano_radius[18:24], ano_radius[24:30], ano_radius[30:36]]
+
+# Set up the figure and axis
+fig, ax = plt.subplots(figsize=(16, 12))
+
+# Loop over grid points and draw circles
+for i in range(grid_size):
+    for j in range(grid_size):
+        # Get the base radius for each point
+        base_radius = radii_ano[j][i]
+        
+        # First circle (centered at (i+1, j+1) because we want labels 1 to 6)
+        circle1 = plt.Circle((2* (i + 1), 1.5*(j + 1)), base_radius, color='blue', fill=False)
+        
+        # Get the misalignment and radius for the second circle
+        misalign_x, misalign_y = cat_missalign[j][i]
+        misalign_radius = radii_cat[j][i]
+        
+        # Second circle (misaligned by (misalign_x, misalign_y))
+        circle2 = plt.Circle((2 * (i + 1) + misalign_x, 1.5 * (j + 1) + misalign_y), misalign_radius, color='red', fill=False)
+
+        # Add circles to plot
+        ax.add_artist(circle1)
+        ax.add_artist(circle2)
+
+        # Plot points for the centers of both circles (smaller markers)
+        ax.plot(2 * (i + 1), 1.5 * (j + 1), 'bo', markersize=3)  # Anode circle center
+        ax.plot(2 * (i + 1) + misalign_x, 1.5 * (j + 1) + misalign_y, 'ro', markersize=3)  # Cathode circle center
+
+# Set limits and aspect ratio
+ax.set_xlim(0, 2 * grid_size + 1)
+ax.set_ylim(0, 1.5 * grid_size + 1)
+ax.set_aspect('equal')
+
+# Set axis labels from 1 to 6 at the correct positions
+ax.set_xticks([2, 4, 6, 8, 10, 12])
+ax.set_xticklabels(range(1, grid_size + 1))
+ax.set_yticks([1.5, 3, 4.5, 6, 7.5, 9])
+ax.set_yticklabels(range(1, grid_size + 1))
+
+# Create legend
+base_patch = mpatches.Patch(color='blue', label='Base')
+second_circle_patch = mpatches.Patch(color='red', label='Second Circle')
+ax.legend(handles=[base_patch, second_circle_patch], loc='upper right')
+
+# Display the plot
+plt.show()
 
 
 
@@ -98,4 +158,4 @@ if not os.path.exists(plot_path):
     os.makedirs(plot_path)
 
 # save plot
-fig.savefig(plot_path + "/" + unique_filename, format='png')
+# fig.savefig(plot_path + "/" + unique_filename, format='png')
