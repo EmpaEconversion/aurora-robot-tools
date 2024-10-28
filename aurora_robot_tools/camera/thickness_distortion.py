04d2765bce5ae5cc2058e53bdc4883c21f62b753
Lina Scholz <lina.sofie.scholz@gmail.com>
2024-10-28 16:45:49 +0100

    finalized all values in pixel which need to be corrected

--- aurora_robot_tools/camera/thickness_distortion.py
+++ aurora_robot_tools/camera/thickness_distortion.py
@@ -14,7 +14,7 @@ desired_width = 1200
 desired_height = 800
 
 # Load image from .h5 file
-with h5py.File("G:/Limit/Lina Scholz/robot_files_20241022/transformed/p01c01s0_p03c02s0_p05c03s0_p02c04s0_p04c05s0_p06c06s0.h5", 'r') as f: 
+with h5py.File("G:/Limit/Lina Scholz/robot_files_gen14/transformed/p01c01s0_p03c02s0_p05c03s0_p02c04s0_p04c05s0_p06c06s0.h5", 'r') as f: 
     content = f['image'][:]
     # In 8-Bit umwandeln
     content = content / np.max(content) * 255
@@ -25,11 +25,11 @@ img = cv2.convertScaleAbs(img, alpha=1.5, beta=0) # 1.5
 img = cv2.GaussianBlur(img, (5, 5), 2) # Apply a Gaussian blur to the image before detecting circles (to improve detection)
 # Apply Hough transform
 detected_circles = cv2.HoughCircles(img,  
-                cv2.HOUGH_GRADIENT, 
-                dp = 1, 
-                minDist = 100, 
-                param1 = 30, param2 = 50, 
-                minRadius = 520, maxRadius = 570)  
+                    cv2.HOUGH_GRADIENT, 
+                    dp = 1, 
+                    minDist = 100, 
+                    param1 = 30, param2 = 50, 
+                    minRadius = 520, maxRadius = 580)  
 
 print(detected_circles)
 
@@ -57,20 +57,20 @@ cv2.imshow("Detected Circles", resized_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 # Store image
-# cv2.imwrite('G:/Limit/Lina Scholz/Python Script/Test images/z-distortion(1).jpg', img)
+# cv2.imwrite('G:/Limit/Lina Scholz/Python Script/Test images/z-distortion(3).jpg', img)
 
 #%% EXTRACTED VALUES TO OFFSET
 
-# 1mm thickness corresponds to:
-pos1 = (0, 0) # TODO
-pos2 = (0, 5.5)
-pos3 = (0, 0) # TODO
+# 1mm thickness corresponds to ... pixel:
+pos1 = (3, 6) # pixel
+pos2 = (0, 6)
+pos3 = (2.5, 6) 
 
 pos4 = (3, 3)
 pos5 = (0, 3)
 pos6 = (2.5, 3)
 
-positions = [pos1, pos2, pos3, pos4, pos5, pos6]
+positions = [pos1, pos2, pos3, pos4, pos5, pos6] # pixel
 
 #%% CORRECT COORDINATE DATA BASE WITH OFFSET (bottom part, separator, spacer)
 
@@ -79,14 +79,14 @@ t_s1 = 0.3 # thickness bottom part
 t_s4 = 1.25 # thickness separator
 t_s7 = 1 # thickness spacer
 
-# step 1:
-corr_s2 = [tuple(x * t_s1 for x in pos) for pos in positions]
-print(corr_s2)
+# from step 1 onwards correct with:
+corr_s1 = [tuple(round(x * t_s1, 3) for x in pos) for pos in positions]
+print(corr_s1)
 
-# step 4:
-corr_s4 = [tuple(x * t_s4 for x in pos) for pos in positions]
+# from step 4 onwards correct with:
+corr_s4 = [tuple(round(x * (t_s1+t_s4), 3) for x in pos) for pos in positions]
 print(corr_s4)
 
-# step 7:
-corr_s7 = [tuple(x * t_s7 for x in pos) for pos in positions]
+# from step 7 onwards correct with:
+corr_s7 = [tuple(round(x * (t_s1+t_s4+t_s7), 3) for x in pos) for pos in positions]
 print(corr_s7)
