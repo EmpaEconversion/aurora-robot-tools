b36c0a40d27590f8f7ee5e9709a780555ee6170b
Lina Scholz <lina.sofie.scholz@gmail.com>
2024-11-18 10:35:41 +0100

    Added function
    storing all alignment numbers vs pressing tool in df

--- aurora_robot_tools/camera/get_alignment.py
+++ aurora_robot_tools/camera/get_alignment.py
@@ -18,9 +18,12 @@ class Alignment:
     def __init__(self, path, graham=False):
         self.path = path # path to images
         self.df = pd.read_excel(os.path.join(path, "data/data.xlsx"), sheet_name="coordinates")
-        self.alignment_df = pd.DataFrame()
+        self.alignment_df = pd.DataFrame(columns=["cell", "press", "x0", "y0", "z0", "x1", "y1", "z1",
+                                                  "x2", "y2", "z2", "x4", "y4", "z4", "x6", "y6", "z6",
+                                                  "x7", "y7", "z7", "x8", "y8", "z8", "x9", "y9", "z9"])
         self.mm_to_pixel = 10
         self.selected_steps = [0, 1, 2, 4, 6, 7, 8, 9]
+        self.unique_cells = self.df['cell'].unique()
 
         # ------------------------------------------------------------------------------------------
         if graham: # Grahams cells
@@ -69,9 +72,10 @@ class Alignment:
         percentage_area = area / (math.pi * R2**2) * 100 # area of cathode overlapping with anode
         return percentage_area
 
-    def plot_coordinates_by_cell(self):
+    def plot_coordinates_by_cell(self, draw_circle=False):
+        """ Add description.
+        """
         # Get unique cell numbers
-        unique_cells = self.df['cell'].unique()
         radii_dict = {0: 20, 1: 20, 2: 15, 4: 16, 5: 16, 6: 14, 7: 19, 8: 16, 9: 20}
         colors = plt.cm.get_cmap('Blues')
         color_range = np.linspace(0.4, 1, len(self.selected_steps))
@@ -80,21 +84,48 @@ class Alignment:
                      6: "cathode", 7: "spacer", 8: "spring", 9: "top"}
 
         # Loop over each cell and create a plot
-        for cell in unique_cells:
+        for cell in self.unique_cells:
             # Filter DataFrame for the current cell and selected steps
-            cell_data = self.df[(self.df['cell'] == cell) & (self.df['step'].isin(self.selected_steps))]
+            cell_df = self.df[(self.df['cell'] == cell) & (self.df['step'].isin(self.selected_steps))]
             # Get the reference point (0,0) for `step 0`
-            ref_x, ref_y = cell_data[cell_data['step'] == 0][['x', 'y']].values[0]
+            ref_x, ref_y = cell_df[cell_df['step'] == 0][['x', 'y']].values[0]
             # Adjust coordinates relative to the step 0 reference point
-            cell_data.loc[:, 'x'] = (cell_data.loc[:, 'x'] - ref_x) / self.mm_to_pixel
-            cell_data.loc[:, 'y'] = (cell_data.loc[:, 'y'] - ref_y) / self.mm_to_pixel
+            cell_df.loc[:, 'x'] = (cell_df.loc[:, 'x'] - ref_x) / self.mm_to_pixel
+            cell_df.loc[:, 'y'] = (cell_df.loc[:, 'y'] - ref_y) / self.mm_to_pixel
+            cell_df["z"] = np.sqrt(cell_df["x"]**2 + cell_df["y"]**2).round(3)
 
-            cell_data["x"]
+            # store all alignments with respect to pressing tool
+            row = {"cell": cell, "press": int(cell_df["press"].values[0]),
+                   "x0": float(cell_df.loc[cell_df['step'] == 0, 'x']),
+                   "y0": float(cell_df.loc[cell_df['step'] == 0, 'y']),
+                   "z0": float(cell_df.loc[cell_df['step'] == 0, 'z']),
+                   "x1": float(cell_df.loc[cell_df['step'] == 1, 'x']),
+                   "y1": float(cell_df.loc[cell_df['step'] == 1, 'y']),
+                   "z1": float(cell_df.loc[cell_df['step'] == 1, 'z']),
+                   "x2": float(cell_df.loc[cell_df['step'] == 2, 'x']),
+                   "y2": float(cell_df.loc[cell_df['step'] == 2, 'y']),
+                   "z2": float(cell_df.loc[cell_df['step'] == 2, 'z']),
+                   "x4": float(cell_df.loc[cell_df['step'] == 4, 'x']),
+                   "y4": float(cell_df.loc[cell_df['step'] == 4, 'y']),
+                   "z4": float(cell_df.loc[cell_df['step'] == 4, 'z']),
+                   "x6": float(cell_df.loc[cell_df['step'] == 6, 'x']),
+                   "y6": float(cell_df.loc[cell_df['step'] == 6, 'y']),
+                   "z6": float(cell_df.loc[cell_df['step'] == 6, 'z']),
+                   "x7": float(cell_df.loc[cell_df['step'] == 7, 'x']),
+                   "y7": float(cell_df.loc[cell_df['step'] == 7, 'y']),
+                   "z7": float(cell_df.loc[cell_df['step'] == 7, 'z']),
+                   "x8": float(cell_df.loc[cell_df['step'] == 8, 'x']),
+                   "y8": float(cell_df.loc[cell_df['step'] == 8, 'y']),
+                   "z8": float(cell_df.loc[cell_df['step'] == 8, 'z']),
+                   "x9": float(cell_df.loc[cell_df['step'] == 9, 'x']),
+                   "y9": float(cell_df.loc[cell_df['step'] == 9, 'y']),
+                   "z9": float(cell_df.loc[cell_df['step'] == 9, 'z'])}
+            self.alignment_df.loc[len(self.alignment_df)] = row
 
             # Create a figure and axis for each cell plot
             fig, ax = plt.subplots(figsize=(10, 10), layout="tight")
             # Loop through each selected step and plot the points with circles
-            for i, (step, row) in enumerate(cell_data.groupby('step')):
+            for i, (step, row) in enumerate(cell_df.groupby('step')):
                 color = colors(color_range[i])
 
                 # Get coordinates and radius
@@ -102,9 +133,10 @@ class Alignment:
                 radius = radii_dict.get(step, 200)  # Default to 200 if step not in dictionary
                 # Plot the center point
                 ax.scatter(x, y, color=color, label=f'{step_name[step]}', s=50, zorder=5)
-                # Add a circle around the center point
-                circle = plt.Circle((x, y), radius, color=color, fill=False, linewidth=1.5)
-                ax.add_patch(circle)
+                if draw_circle:
+                    # Add a circle around the center point
+                    circle = plt.Circle((x, y), radius, color=color, fill=False, linewidth=1.5)
+                    ax.add_patch(circle)
 
                 # Annotate with step label, offset from center
                 ax.annotate(
@@ -134,12 +166,23 @@ class Alignment:
             plt.savefig(os.path.join(data_dir, f"center_cell_{cell}.jpg"), format="jpg")
             plt.close()
 
+        # Save alignment data frame
+        self.alignment_df = self.alignment_df.sort_values(by='cell')
+        data_dir = os.path.join(self.path, "data")
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+        with pd.ExcelWriter(os.path.join(data_dir, "alignment.xlsx")) as writer:
+            self.alignment_df.to_excel(writer, sheet_name='alignment', index=False)
+        self.alignment_df.to_csv(os.path.join(data_dir, "alignment.csv"), index=False)
+
+        return self.alignment_df
+
     def plot_differences(self, step, name):
         # create folder plot if not existent
         if not os.path.exists('plot'):
             os.makedirs('plot')
 
-        df_filtered = self.df[self.df['step'].isin([0, 2])] # filter for specific steps
+        df_filtered = self.df[self.df['step'].isin([0, step])] # filter for specific steps
         # pivot data frame to have step 0 and 2 next to each other
         df_pivot = df_filtered.pivot_table(index='cell', columns='step', values=['x', 'y'])
         # calculate difference
@@ -153,6 +196,7 @@ class Alignment:
         ax.set_xlabel('Cell Number / Rack Position')
         ax.set_ylabel('Misalignment [mm]')
         ax.set_title(f'{name} Misalignment')
+        ax.set_ylim([-2.5, 2.5])
         ax.legend()
         ax.grid(True)
 
@@ -160,7 +204,7 @@ class Alignment:
         data_dir = os.path.join(self.path, "plot")
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
-        plt.savefig(os.path.join(data_dir, 'differences_plot_anode.png'), format="jpg")
+        plt.savefig(os.path.join(data_dir, f'differences_plot_{name}.png'), format="jpg")
         plt.close()
 
     def compute_differences(self):
@@ -200,7 +244,10 @@ if __name__ == '__main__':
     obj = Alignment(folderpath)
     obj.plot_coordinates_by_cell()
     obj.plot_differences(step=2, name="Anode")
-    differences_matrix = obj.compute_differences()
-    obj.plot_correlation_matrix(differences_matrix)
+    obj.plot_differences(step=6, name="Cathode")
+    obj.plot_differences(step=7, name="Spacer")
+    obj.plot_differences(step=8, name="Spring")
+    # differences_matrix = obj.compute_differences()
+    # obj.plot_correlation_matrix(differences_matrix)
 
 
