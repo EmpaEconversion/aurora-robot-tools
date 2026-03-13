26149f46719afdc906a96d008f54dea6465ceea9
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-03-13 16:51:58 +0100

    Update tests

--- tests/test_import_excel.py
+++ tests/test_import_excel.py
@@ -1,13 +1,9 @@
 """Test functions from import_excel.py."""
 
-import sqlite3
-from pathlib import Path
-from unittest.mock import patch
-
 import numpy as np
 import pandas as pd
 
-from aurora_robot_tools.import_excel import fix_mixed_batches, main
+from aurora_robot_tools.import_excel import fix_mixed_batches
 
 
 def test_fix_mixed_batches() -> None:
@@ -51,28 +47,3 @@ def test_fix_mixed_batches() -> None:
         np.array([np.nan, np.nan, 1, 2, 2, 3, 4, 3, 5]),
         equal_nan=True,
     )
-
-
-def test_excel_import(test_dir: Path, tmp_path: Path) -> None:
-    """Test full import."""
-    fake_xlsx = test_dir / "date_name_tag.xlsx"
-    with patch("aurora_robot_tools.import_excel.filedialog.askopenfilename", return_value=str(fake_xlsx)):
-        main()
-        db_path = tmp_path / "test.db"
-        assert db_path.exists()
-        with sqlite3.connect(db_path) as conn:
-            cur = conn.cursor()
-            db_map = {}
-            tables = cur.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
-            for (table,) in tables:
-                cols = cur.execute(f"PRAGMA table_info({table})").fetchall()
-                col_names = [row[1] for row in cols]
-                db_map[table] = col_names
-        assert "Cell_Assembly_Table" in db_map
-        assert "Electrolyte_Table" in db_map
-        assert "Timestamp_Table" in db_map
-        assert set(db_map["Timestamp_Table"]) == {"Cell Number", "Step Number", "Timestamp", "Complete"}
-
-        assert "Settings_Table" in db_map
-        assert "Calibration_Table" in db_map
-        assert set(db_map["Calibration_Table"]) == {"Cell Number", "Step Number", "Rack Position", "dx_mm", "dy_mm"}
