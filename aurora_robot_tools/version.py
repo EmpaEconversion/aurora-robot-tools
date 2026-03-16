074b169a1309ba0a1af461e99528988b175ec936
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-03-16 18:05:47 +0100

    Bump version v0.3.3 -> v0.3.4

--- aurora_robot_tools/version.py
+++ aurora_robot_tools/version.py
@@ -1,6 +1,6 @@
 """Version information for aurora-robot-tools."""
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __author__ = "Graham Kimbell"
 __title__ = "Aurora robot tools"
 __description__ = """
